87b6cc2291265fb126347a4c2972667d11ab910d
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-15 11:24:09 +0200

    refractor
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

--- PhotoTools/Utils/Constant/Directory.cs
+++ PhotoTools/Utils/Constant/Directory.cs
@@ -6,7 +6,7 @@ public static class Directory
 {
     public const string GithubPage = @"https://github.com/TheR7angelo/PhotoTools";
     
-    public static readonly string BddMain = $"{GetPath.GetCurrentPath}\\Sql\\Bdds\\Main.sqlite";
-    public static readonly string BddLanguage = $"{GetPath.GetCurrentPath}\\Sql\\Bdds\\Language.sqlite";
-    public static readonly string BddProgress = $"{GetPath.GetCurrentPath}\\Sql\\Bdds\\Progress.sqlite";
+    public static readonly string BddMain = $"{GetPath.GetCurrentPath}\\Utils\\Sql\\Bdds\\Main.sqlite";
+    public static readonly string BddLanguage = $"{GetPath.GetCurrentPath}\\Utils\\Sql\\Bdds\\Language.sqlite";
+    public static readonly string BddProgress = $"{GetPath.GetCurrentPath}\\Utils\\Sql\\Bdds\\Progress.sqlite";
 }
