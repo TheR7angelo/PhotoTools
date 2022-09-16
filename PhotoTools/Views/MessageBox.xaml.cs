b86ab3927c77fc018cb0eeb94d1e6a1d0deb8f4b
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-16 08:19:01 +0200

    added todo
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

--- PhotoTools/Views/MessageBox.xaml.cs
+++ PhotoTools/Views/MessageBox.xaml.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using PhotoTools.Utils.Strucs;
 
 namespace PhotoTools.Views.Settings;
 
