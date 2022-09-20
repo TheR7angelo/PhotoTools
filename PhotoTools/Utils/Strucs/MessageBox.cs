6cd3784f1ecc17e9d3859dfdd77e37c6298d61ee
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-20 15:09:37 +0200

    ras
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

--- PhotoTools/Utils/Strucs/MessageBox.cs
+++ PhotoTools/Utils/Strucs/MessageBox.cs
@@ -19,7 +19,7 @@ public class MessageBox
 
         public ImageSource Check { get; } = (ImageSource)Application.Current.Resources["BasicUi008-Check"];
         public ImageSource Information { get; } = (ImageSource)Application.Current.Resources["BasicUi050-Info"];
-        public ImageSource Stop { get; } = (ImageSource)Application.Current.Resources["BasicUi076-Remove"];
+        public ImageSource Error { get; } = (ImageSource)Application.Current.Resources["BasicUi076-Remove"];
         public ImageSource Question { get; } = (ImageSource)Application.Current.Resources["BasicUi073-Question"];
         public ImageSource Warning { get; } = (ImageSource)Application.Current.Resources["BasicUi091-Warning"];
     }
