f46d830c242a29cc10cdd7025b0480cf0db9eaf0
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-20 20:08:14 +0200

    added list images BasicUi
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

---
+++ PhotoTools/Utils/Img/Pack/BasicUi/BasicUi.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using PhotoTools.Utils.Strucs;
+
+namespace PhotoTools.Utils.Img.Pack;
+
+public partial class Image
+{
+    private Images.LicenceImages _imageBasicUi = new()
+    {
+        Url = "https://www.flaticon.com/packs/basic-ui-3?word=basic%20ui",
+        Images = new List<string>
+        {
+            "BasicUi008-Check", "BasicUi050-Info", "BasicUi067-Plus", "BasicUi073-Question", "BasicUi076-Remove",
+            "BasicUi089-Trash", "BasicUi091-Warning"
+        }
+    };
+}
---
+++ PhotoTools/Utils/Strucs/Image.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PhotoTools.Utils.Strucs;
+
+public static class Images
+{
+    public struct LicenceImages
+    {
+        public string Url;
+        public List<string> Images;
+    }
+}
