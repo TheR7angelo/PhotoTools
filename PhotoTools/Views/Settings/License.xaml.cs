31535d90fe6c8cbe98445b3826f73a298aabc46e
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-20 19:54:38 +0200

    added licence show image retribution
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

---
+++ PhotoTools/Views/Settings/License.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace PhotoTools.Views.Settings;
+
+public partial class License : UserControl
+{
+    public License()
+    {
+        InitializeComponent();
+    }
+}
