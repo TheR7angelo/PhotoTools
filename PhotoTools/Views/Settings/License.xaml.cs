a4d60543c152c776448626df3f03a115f42324f5
TheR7angelo <raphael.antunes@homtail.fr>
2022-09-21 14:53:23 +0200

    added todo

--- PhotoTools/Views/Settings/License.xaml.cs
+++ PhotoTools/Views/Settings/License.xaml.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using PhotoTools.Utils.Strucs;
 
 namespace PhotoTools.Views.Settings;
