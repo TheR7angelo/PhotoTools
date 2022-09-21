cf49af9ea4bb27206f0f6ea1b9ce3deda669f7f6
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-21 23:59:50 +0200

    added function to parse data type to another data type
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

---
+++ PhotoTools/Utils/Function/Converter/DataType.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PhotoTools.Utils.Function;
+
+public static partial class Convert
+{
+
+    #region bool
+
+    public static bool ParseToBool(this string str)
+    {
+        return str.ParseToInt() > 0;
+    }
+    
+    public static bool ParseToBool(this int i)
+    {
+        return i > 0;
+    }
+
+    #endregion
+
+    #region int
+
+    public static int ParseToInt(this string str)
+    {
+        if (int.TryParse(str, out _))
+        {
+            return int.Parse(str);
+        }
+        throw new InvalidOperationException("cannot parse to int");
+    }
+
+    public static int ParseToInt(this bool bo)
+    {
+        return bo ? 1 : 0;
+    }
+
+    #endregion
+}
