ba0f0a290701166f6c147c9abd9b3bc5f2c845dd
TheR7angelo <raphael.antunes@homtail.fr>
2022-09-21 09:41:25 +0200

    GetImageSourceResources function added

--- PhotoTools/Utils/Function/Getter/Application.cs
+++ PhotoTools/Utils/Function/Getter/Application.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Reflection;
+using System.Windows.Media;
 
 namespace PhotoTools.Utils.Function;
 
-public static partial class Get
+public partial class Get
 {
     private static AssemblyName _assemblyName = Assembly.GetExecutingAssembly().GetName();
     public static string? GetCurrentName()
@@ -11,4 +12,9 @@ public static partial class Get
         return _assemblyName.Name;
     }
     public static Version? GetCurrentVersion { get; } = _assemblyName.Version;
+
+    public static ImageSource GetImageSourceResources(string key)
+    {
+        return (System.Windows.Application.Current.Resources[key] as ImageSource)!;
+    }
 }
