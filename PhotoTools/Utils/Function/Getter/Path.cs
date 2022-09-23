6e98862ff6b96d2eb82db342ed22220894921517
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-23 12:10:33 +0200

    added default path Desktop, Documents
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

--- PhotoTools/Utils/Function/Getter/Path.cs
+++ PhotoTools/Utils/Function/Getter/Path.cs
@@ -4,5 +4,8 @@ namespace PhotoTools.Utils.Function;
 
 public partial class Get
 {
-    public static string GetCurrentPath { get; } = Environment.CurrentDirectory;
+    public static string GetCurrentPath => Environment.CurrentDirectory;
+    
+    public static string GetDesktop => Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+    public static string GetDocuments => Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 }
