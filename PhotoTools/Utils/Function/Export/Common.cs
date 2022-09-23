dd9e0f3fc7d3be3fdfda742129871ce732832bef
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-23 18:32:14 +0200

    added title to export file
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

--- PhotoTools/Utils/Function/Export/Common.cs
+++ PhotoTools/Utils/Function/Export/Common.cs
@@ -2,27 +2,29 @@
 
 namespace PhotoTools.Utils.Function;
 
-public partial class Export
+public static partial class Export
 {
-    public static string SaveFile(string? initialFolder = null, string[]? filter = null)
+    public static string SaveFile(string title, string? initialFolder = null, string[]? filter = null)
     {
         var ifilter = filter is null ? string.Empty : string.Join('|', filter);
-        var iinitialFolder = initialFolder is null ? string.Empty : string.Join('|', initialFolder);
+        var iinitialFolder = initialFolder is null ? string.Empty : initialFolder;
 
         // "Text file (*.txt)|*.txt|C# file (*.cs)|*.cs"
         var saveFileDialog = new SaveFileDialog {
+            Title = title,
             Filter = ifilter,
             InitialDirectory = iinitialFolder
         };
         return saveFileDialog.ShowDialog() != true ? string.Empty : saveFileDialog.FileName;
     }
     
-    public static string SaveFile(string? initialFolder = null, string? filter = null)
+    public static string SaveFile(string title, string? initialFolder = null, string? filter = null)
     {
-        var ifilter = filter is null ? string.Empty : string.Join('|', filter);
-        var iinitialFolder = initialFolder is null ? string.Empty : string.Join('|', initialFolder);
+        var ifilter = filter is null ? string.Empty : filter;
+        var iinitialFolder = initialFolder is null ? string.Empty : initialFolder;
         
         var saveFileDialog = new SaveFileDialog {
+            Title = title,
             Filter = ifilter,
             InitialDirectory = iinitialFolder
         };
