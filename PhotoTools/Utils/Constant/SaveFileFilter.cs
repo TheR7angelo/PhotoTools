7c1c0b412b2000ddd802b49342329ec736a4088c
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-24 15:13:21 +0200

    added filtre parameter
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

--- PhotoTools/Utils/Constant/SaveFileFilter.cs
+++ PhotoTools/Utils/Constant/SaveFileFilter.cs
@@ -2,5 +2,20 @@
 
 public static class SaveFileFilter
 {
-    public static string Json() => "Json file (*.json)|*.json";
+    public static Filter SemiColonCsv => new()
+        { Value = "Csv (délimiter par des points virgule) (*.csv)|*.csv", Extension = ".csv" };
+
+    public static Filter CommaCsv => new()
+        { Value = "Csv (délimiter par une virgule) (*.csv)|*.csv", Extension = ".csv" };
+    
+    public static Filter Json => new () { Value = "Json file (*.json)|*.json", Extension = ".json" };
+
+    public struct Filter
+    {
+        public string Value = string.Empty;
+        public string Extension = string.Empty;
+
+        public Filter(){}
+    }
+    
 }
