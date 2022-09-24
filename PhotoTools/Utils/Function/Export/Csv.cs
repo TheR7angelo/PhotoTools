11126eb25cc6273aed7e10bad4624e2fe1d4604b
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-24 15:12:59 +0200

    added csv export
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

---
+++ PhotoTools/Utils/Function/Export/Csv.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using PhotoTools.Utils.Strucs;
+
+namespace PhotoTools.Utils.Function;
+
+public static partial class Export
+{
+    public static void ExportCsv(string path, StrucConfig.Themes themes, char delimiter)
+    {
+        var columns = new List<string> { "name" };
+        var colors = new List<string>{ themes.Name! };
+
+        foreach (var color in themes.Value)
+        {
+            columns.Add(color.Name);
+            colors.Add(color.StyleValue.ToString());
+        }
+
+        var csv = new List<List<string>>{ columns, colors };
+        var writer =  new StreamWriter(path);
+
+        WriteCsv(csv, delimiter, writer);
+        
+        writer.Close();
+    }
+
+    private static void WriteCsv(IEnumerable<IEnumerable<object>> lines, char delimiter, TextWriter writer)
+    {
+        // todo make new line ;)
+        foreach (var line in lines)
+        {
+            WriteCsv(line, delimiter, writer);
+        }
+    }
+    
+    private static void WriteCsv(IEnumerable<object> lines, char delimiter, TextWriter writer)
+    {
+        var iline = lines.Select(line => line.ToString()!).ToList();
+        writer.Write(string.Join(delimiter, iline));
+    }
+}
