5b552a3bd5cef409399b6b3118e925a8179cf03f
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-24 15:12:41 +0200

    change filter using
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

--- PhotoTools/Utils/Function/Export/Common.cs
+++ PhotoTools/Utils/Function/Export/Common.cs
@@ -1,27 +1,43 @@
-﻿using Microsoft.Win32;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Win32;
+using PhotoTools.Utils.Constant;
 
 namespace PhotoTools.Utils.Function;
 
 public static partial class Export
 {
-    public static string SaveFile(string title, string? initialFolder = null, string[]? filter = null)
+    public static (string, string) SaveFile(string title, string? initialFolder = null, List<SaveFileFilter.Filter>? filters = null)
     {
-        var ifilter = filter is null ? string.Empty : string.Join('|', filter);
-        var iinitialFolder = initialFolder is null ? string.Empty : initialFolder;
-
-        // "Text file (*.txt)|*.txt|C# file (*.cs)|*.cs"
+        var ifilter = filters is null ? string.Empty : filters.Aggregate("", (current, filter) => $"{current}|{filter}");
+        var iinitialFolder = initialFolder ?? string.Empty;
+        
         var saveFileDialog = new SaveFileDialog {
             Title = title,
             Filter = ifilter,
             InitialDirectory = iinitialFolder
         };
-        return saveFileDialog.ShowDialog() != true ? string.Empty : saveFileDialog.FileName;
+        
+        if (saveFileDialog.ShowDialog() is not true) return (string.Empty, string.Empty);
+
+        if (!ifilter.Equals(string.Empty)) return (saveFileDialog.FileName, filters![saveFileDialog.FilterIndex].Value);
+        {
+            var use = string.Empty;
+            var ext = Path.GetExtension(saveFileDialog.FileName);
+            foreach (var filter in filters!.Where(filter => filter.Extension.Equals(ext)))
+            {
+                use = filter.Value;
+                break;
+            }
+            return (saveFileDialog.FileName, use);
+        }
     }
     
-    public static string SaveFile(string title, string? initialFolder = null, string? filter = null)
+    public static string SaveFile(string title, string? initialFolder = null, SaveFileFilter.Filter? filter = null)
     {
-        var ifilter = filter is null ? string.Empty : filter;
-        var iinitialFolder = initialFolder is null ? string.Empty : initialFolder;
+        var ifilter = filter is null ? string.Empty : filter.Value.Value;
+        var iinitialFolder = initialFolder ?? string.Empty;
         
         var saveFileDialog = new SaveFileDialog {
             Title = title,
