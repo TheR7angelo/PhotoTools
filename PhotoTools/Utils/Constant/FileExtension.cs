f76d93915e14e38b37c7fc635e8a676b3fd7fe3a
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-25 12:29:18 +0200

    added const extension or delimiter for file
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

---
+++ PhotoTools/Utils/Constant/FileExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace PhotoTools.Utils.Constant;
+
+public static class FileExtension
+{
+    public const string Csv = ".csv";
+    public const char Semicolon = ';';
+    public const char Comma = ',';
+    
+    public const string Json = ".json";
+}
