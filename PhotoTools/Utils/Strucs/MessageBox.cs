36dd3c39573a64a9e751b866534a2a02028e67ba
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-29 10:25:52 +0200

    Ad function to call imageSource
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

--- PhotoTools/Utils/Strucs/MessageBox.cs
+++ PhotoTools/Utils/Strucs/MessageBox.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Media;
+using PhotoTools.Utils.Function;
 
 namespace PhotoTools.Utils.Strucs;
 
@@ -17,10 +18,10 @@ public static class MessageBox
         {
         }
 
-        public ImageSource Check { get; } = (ImageSource)Application.Current.Resources["BasicUi008-Check"];
-        public ImageSource Information { get; } = (ImageSource)Application.Current.Resources["BasicUi050-Info"];
-        public ImageSource Error { get; } = (ImageSource)Application.Current.Resources["BasicUi076-Remove"];
-        public ImageSource Question { get; } = (ImageSource)Application.Current.Resources["BasicUi073-Question"];
-        public ImageSource Warning { get; } = (ImageSource)Application.Current.Resources["BasicUi091-Warning"];
+        public ImageSource Check { get; } = "BasicUi008-Check".GetResourcesImageSource();// (ImageSource)Application.Current.Resources["BasicUi008-Check"];
+        public ImageSource Information { get; } = "BasicUi050-Info".GetResourcesImageSource();//(ImageSource)Application.Current.Resources["BasicUi050-Info"];
+        public ImageSource Error { get; } = "BasicUi076-Remove".GetResourcesImageSource();//(ImageSource)Application.Current.Resources["BasicUi076-Remove"];
+        public ImageSource Question { get; } = "BasicUi073-Question".GetResourcesImageSource();//(ImageSource)Application.Current.Resources["BasicUi073-Question"];
+        public ImageSource Warning { get; } = "BasicUi091-Warning".GetResourcesImageSource();//(ImageSource)Application.Current.Resources["BasicUi091-Warning"];
     }
 }
