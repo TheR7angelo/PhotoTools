478b173b91812471634436b82e2904d3cceb531f
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-29 10:27:27 +0200

    refractor
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

--- PhotoTools/Utils/Function/Getter/Application.cs
+++ PhotoTools/Utils/Function/Getter/Application.cs
@@ -4,17 +4,12 @@ using System.Windows.Media;
 
 namespace PhotoTools.Utils.Function;
 
-public partial class Get
+public static partial class Get
 {
-    private static AssemblyName _assemblyName = Assembly.GetExecutingAssembly().GetName();
-    public static string? GetCurrentName()
-    {
-        return _assemblyName.Name;
-    }
-    public static Version? GetCurrentVersion { get; } = _assemblyName.Version;
+    private static readonly AssemblyName AssemblyName = Assembly.GetExecutingAssembly().GetName();
+    
+    public static string? GetCurrentName() => AssemblyName.Name;
+    public static Version? GetCurrentVersion { get; } = AssemblyName.Version;
 
-    public static ImageSource GetResourcesImageSource(string key)
-    {
-        return (System.Windows.Application.Current.Resources[key] as ImageSource)!;
-    }
+    public static ImageSource GetResourcesImageSource(this string key) => (System.Windows.Application.Current.Resources[key] as ImageSource)!;
 }
