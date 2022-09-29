fe8fb78409c290610bc4ae51763fb7c0a14deb50
ZP6177 <raphael.antunes@hotmail.fr>
2022-09-29 10:27:03 +0200

    Added new parser: Bitmap to ImageSource
    Signed-off-by: ZP6177 <raphael.antunes@hotmail.fr>

---
+++ PhotoTools/Utils/Function/Converter/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace PhotoTools.Utils.Function;
+
+public static partial class Convert
+{
+    public static ImageSource ParseToImageSource(this Bitmap bitmap) => Imaging.CreateBitmapSourceFromHBitmap
+        (bitmap.GetHbitmap(), IntPtr.Zero,Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+}
